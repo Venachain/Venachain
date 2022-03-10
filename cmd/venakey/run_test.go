1768dec182d34b30d3c03663a541db31ed7cfed1
zhangwei <zhangwei@zhangweideMacBook-Pro.local>
2022-03-10 15:42:01 +0800

    rename ethkey to venakey

--- cmd/venakey/run_test.go
+++ cmd/venakey/run_test.go
@@ -25,21 +25,21 @@ import (
 	"github.com/docker/docker/pkg/reexec"
 )
 
-type testEthkey struct {
+type testVenakey struct {
 	*cmdtest.TestCmd
 }
 
-// spawns ethkey with the given command line args.
-func runEthkey(t *testing.T, args ...string) *testEthkey {
-	tt := new(testEthkey)
+// spawns venakey with the given command line args.
+func runVenakey(t *testing.T, args ...string) *testVenakey {
+	tt := new(testVenakey)
 	tt.TestCmd = cmdtest.NewTestCmd(t, tt)
-	tt.Run("ethkey-test", args...)
+	tt.Run("venakey-test", args...)
 	return tt
 }
 
 func TestMain(m *testing.M) {
-	// Run the app if we've been exec'd as "ethkey-test" in runEthkey.
-	reexec.Register("ethkey-test", func() {
+	// Run the app if we've been exec'd as "venakey-test" in runVenakey.
+	reexec.Register("venakey-test", func() {
 		if err := app.Run(os.Args); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
