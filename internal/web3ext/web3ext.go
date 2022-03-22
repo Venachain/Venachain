e7120ea068602a13760fa7d44b4ff0616ffc309c
zhangwei <zhangwei@zhangweideMacBook-Pro.local>
2022-03-22 15:58:19 +0800

    rename istanbul namespace to iris namespace

--- internal/web3ext/web3ext.go
+++ internal/web3ext/web3ext.go
@@ -30,7 +30,7 @@ var Modules = map[string]string{
 	"shh":        Shh_JS,
 	"swarmfs":    SWARMFS_JS,
 	"txpool":     TxPool_JS,
-	"istanbul":   Istanbul_JS,
+	"iris":       Iris_JS,
 	"venachain":  Venachain_JS,
 }
 
@@ -730,18 +730,18 @@ web3._extend({
 });
 `
 
-const Istanbul_JS = `
+const Iris_JS = `
 web3._extend({
-	property: 'istanbul',
+	property: 'iris',
 	methods: [
 		new web3._extend.Method({
 			name: 'getValidators',
-			call: 'istanbul_getValidators',
+			call: 'iris_getValidators',
 			params: 0
 		}),
 		new web3._extend.Method({
 			name: 'getCandidates',
-			call: 'istanbul_candidates',
+			call: 'iris_candidates',
 			params: 0
 		}),
 	],
