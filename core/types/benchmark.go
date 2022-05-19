d1bc03510f59215a5bf5826ff1454c018c2ca7be
曾梦露 <zengmenglu@wx.bc>
2022-05-19 21:03:31 +0800

    Revert "performance: add evm preGenerate"
    This reverts commit 969899b7f88f0fd178e66928b6219afd859425e3

--- core/types/benchmark.go
+++ core/types/benchmark.go
@@ -1,9 +1,8 @@
 package types
 
 type Benchmark struct {
-	To           string `json:"to" gencodec:"required"`
-	Count        string `json:"count" gencodec:"required"`
-	PreGenerate  bool   `json:"preGenerate" gencodec:"required"`
-	ProducerCnt  string `json:"producerCnt" gencodec:"required"`
-	ContractType string `json:"contractType" gencodec:"required"`
+	To          string `json:"to" gencodec:"required"`
+	Count       string `json:"count" gencodec:"required"`
+	PreGenerate bool   `json:"preGenerate" gencodec:"required"`
+	ProducerCnt string `json:"producerCnt" gencodec:"required"`
 }
